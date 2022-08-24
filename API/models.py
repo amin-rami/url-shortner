aed5d0e00c0d90240d48db80e9fef6499c2fa943
amin-rami <ma.rami@yahoo.com>
2022-08-24 11:44:27 +0430

    add url model and expand url shortner api

--- API/models.py
+++ API/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    long_url = models.CharField(max_length=500, primary_key=True)
+    short_url = models.CharField(max_length=500)
+    clicks = models.IntegerField()
+    time_created = models.CharField(max_length=100)
+
+    
--- API/views.py
+++ API/views.py
@@ -1,17 +1,23 @@
+import json
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpRequest
+import json
 
 
 def shortner_api(request: HttpRequest):
     params = dict(request.GET)
+    response_data = {'massege': ''}
+
     try:
         long_url = params.pop('url')
         if params:
             raise Exception
     except:
-        return HttpResponseBadRequest('Bad Request')
+        response_data['massege'] = 'Bad request format'
+        return HttpResponseBadRequest(json.dumps(response_data))
 
-        
+
     return HttpResponse('Success!')
 
 
