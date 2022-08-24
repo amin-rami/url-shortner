701df4b0b18746b7068bf5731c841e91f0ba9783
amin-rami <ma.rami@yahoo.com>
2022-08-24 18:48:46 +0430

    MVP

--- API/views.py
+++ API/views.py
@@ -1,22 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import string
 import random
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpRequest, HttpResponseNotFound, HttpResponseRedirect
-import json
 from django.urls import reverse
 from .models import Url
 from rest_framework.decorators import api_view
+from .urls import urlpatterns 
 
+time_diff = timedelta(hours=4, minutes = 30)
 
 @api_view(['POST', 'GET'])
 def shortner_api(request: HttpRequest):
 
     body_unicode = request.body.decode('utf-8')
     params = dict(json.loads(body_unicode))
-    response_data = {'massege': ''}
+    response_data = {'message': ''}
     DOMAIN = 'localhost:8000/api/short/'  # can be imporved
 
     try:
@@ -24,36 +25,35 @@ def shortner_api(request: HttpRequest):
         if params:
             raise Exception
     except:
-        response_data['massege'] = 'Bad request format'
+        response_data['message'] = 'Bad request format'
         return HttpResponseBadRequest(json.dumps(response_data))
-
 
     short_url = short_url_create()
     clicks = 0
-    now = datetime.now()
+    now = datetime.now() + time_diff
     now = now.strftime("%Y/%m/%d %H:%M")
     url = Url(long_url=long_url, short_url=short_url,
-                clicks=clicks, time_created=now)
+              clicks=clicks, time_created=now)
     url.save()
-    response_data['massege'] = 'Success! returning a new url'
+    response_data['message'] = 'Success!'
     response_data['short_url'] = DOMAIN + short_url
-    response_data['is_new'] = True
 
     return HttpResponse(json.dumps(response_data))
 
+
 @api_view(['POST', 'GET'])
 def redirect_api(request: HttpRequest, short_url):
-    response_data = {'massege': ''}
+    response_data = {'message': ''}
 
     url_exists = Url.objects.filter(short_url=short_url).exists()
 
     if not url_exists:
-        response_data['massege'] = 'Such short url does not exist'
+        response_data['message'] = 'Such short url does not exist'
         return HttpResponseNotFound(json.dumps(response_data))
 
     url = Url.objects.get(short_url=short_url)
     url.clicks += 1
-    now = datetime.now()
+    now = datetime.now() + time_diff
     now = now.strftime("%Y/%m/%d %H:%M")
     url.last_access = now
     url.save()
